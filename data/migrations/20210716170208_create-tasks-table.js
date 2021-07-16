e9257bba37018db576eec5fbf957dd48632160a0
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-16 10:09:05 -0700

    create tasks migration

---
+++ data/migrations/20210716170208_create-tasks-table.js
@@ -0,0 +1,20 @@
+exports.up = async function (knex) {
+  await knex.schema.createTable("tasks", (table) => {
+    table.increments("task_id");
+    table.string("task_description").notNullable();
+    table.text("task_notes");
+    table.boolean("task_completed").defaultTo(false);
+    table
+      .integer("project_id")
+      .unsigned()
+      .notNullable()
+      .references("project_id")
+      .inTable("projects")
+      .onDelete("RESTRICT")
+      .onUpdate("RESTRICT");
+  });
+};
+
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("tasks");
+};
