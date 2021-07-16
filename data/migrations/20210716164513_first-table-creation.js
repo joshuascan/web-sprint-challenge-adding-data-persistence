06bc637a7ae14b081f64ef25b2dce9b5a0323f42
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-16 13:57:41 -0700

    MVP

--- data/migrations/20210716164513_first-table-creation.js
+++ data/migrations/20210716164513_first-table-creation.js
@@ -1,8 +1,2 @@
-exports.up = async function (knex) {
-  await knex.schema.createTable("test", (table) => {
-    table.integer("test");
-  });
-};
-exports.down = async function (knex) {
-  await knex.schema.dropTableIfExists("test");
-};
+exports.up = async function (knex) {};
+exports.down = async function (knex) {};
