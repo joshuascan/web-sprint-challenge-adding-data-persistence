17d572842333ffb064ecda0a81fea5d38e2e3f10
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-16 13:55:32 -0700

    codegrade test2

--- data/migrations/20210716164513_first-table-creation.js
+++ data/migrations/20210716164513_first-table-creation.js
@@ -1,3 +1,8 @@
-exports.up = function (knex) {};
-
-exports.down = function (knex) {};
+exports.up = async function (knex) {
+  await knex.schema.createTable("test", (table) => {
+    table.integer("test");
+  });
+};
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("test");
+};
