fb45389326e98c74cfbafb9d92f1eba3247bf1d1
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-16 11:41:00 -0700

    creates post handler for resource creation

--- api/resource/model.js
+++ api/resource/model.js
@@ -4,6 +4,12 @@ async function find() {
   return db("resources");
 }
 
-function add(resource) {}
+function add(resource) {
+  return db("resources")
+    .insert(resource)
+    .then(([resource_id]) => {
+      return db("resources").where("resource_id", resource_id).first();
+    });
+}
 
 module.exports = { find, add };
