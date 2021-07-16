b45a3bb966ae7549424d15105f6fd6f74fc4db0c
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-16 14:12:16 -0700

    MVP

--- api/server.js
+++ api/server.js
@@ -7,15 +7,15 @@ const taskRouter = require("./task/router");
 const server = express();
 
 server.use(express.json());
-// server.use("/api/projects", projectRouter);
-// server.use("/api/resources", resourceRouter);
-// server.use("/api/tasks", taskRouter);
+server.use("/api/projects", projectRouter);
+server.use("/api/resources", resourceRouter);
+server.use("/api/tasks", taskRouter);
 
-// server.use((err, req, res, next) => {
-//   res.status(500).json({
-//     message: err.message,
-//     stack: err.stack,
-//   });
-// });
+server.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
 
 module.exports = server;
