ac5c6dc2caf8e770bceba19ac43f56a70d921a35
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-16 09:58:14 -0700

    create projects migration

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,13 @@
-// build your `/api/projects` router here
+const express = require("express");
+const Project = require("./model");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  try {
+    res.status(200).json({ message: "get all projects" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
