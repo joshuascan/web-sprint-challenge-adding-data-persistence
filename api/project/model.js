91f830a0e9c287bda761eb1d0653f9fefe92a38b
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-16 12:16:21 -0700

    cleans up code for add models

--- api/project/model.js
+++ api/project/model.js
@@ -14,18 +14,14 @@ async function find() {
   return result;
 }
 
-function add(project) {
-  return db("projects")
-    .insert(project)
-    .then(async ([project_id]) => {
-      const newProject = await db("projects")
-        .where("project_id", project_id)
-        .first();
-      return {
-        ...newProject,
-        project_completed: newProject.project_completed === 1 ? true : false,
-      };
-    });
+async function add(project) {
+  const [id] = await db("projects").insert(project);
+
+  const newProject = await db("projects").where("project_id", id).first();
+  return {
+    ...newProject,
+    project_completed: newProject.project_completed === 1 ? true : false,
+  };
 }
 
 module.exports = { find, add };
